d940c5be9c16e3e7ffb4f6440494c8fb8da22244
Bruno Sanches <bcsanches@gmail.com>
2018-11-03 23:05:34 -0300

    Fixed log to write to file and to msvc debug output

--- src/Common/src/LogUtils.cpp
+++ src/Common/src/LogUtils.cpp
@@ -2,42 +2,53 @@
 
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/msvc_sink.h>
 
 namespace dcclite
 {
+	static std::shared_ptr<spdlog::logger> g_spLogger;
+
 	//https://github.com/SergiusTheBest/plog#logger
 	void LogInit(const char *fileName)
 	{
-		// create color multi threaded logger
-		auto console = spdlog::stdout_color_mt("dcclite");
-		console->info("Welcome to spdlog!");
-		console->error("Some error message with arg: {}", 1);
+		//auto console = spdlog::stdout_color_mt("console");
 
-		auto err_logger = spdlog::stderr_color_mt("stderr");
-		err_logger->error("Some error message");
+		std::vector<spdlog::sink_ptr> sinks;
+		sinks.push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+		sinks.push_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>(fileName, true));
+		sinks.push_back(std::make_shared<spdlog::sinks::msvc_sink_mt>());
+		auto combined_logger = std::make_shared<spdlog::logger>("dcclite", begin(sinks), end(sinks));
+		//register it if you need to access it globally
+		spdlog::register_logger(combined_logger);		
+		
+		//console->info("Welcome to spdlog!");
+		//console->error("Some error message with arg: {}", 1);		
 
 		// Formatting examples
-		console->warn("Easy padding in numbers like {:08d}", 12);
-		console->critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
-		console->info("Support for floats {:03.2f}", 1.23456);
-		console->info("Positional args are {1} {0}..", "too", "supported");
-		console->info("{:<30}", "left aligned");
+		//console->warn("Easy padding in numbers like {:08d}", 12);
+		//console->critical("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+		//console->info("Support for floats {:03.2f}", 1.23456);
+		//console->info("Positional args are {1} {0}..", "too", "supported");
+		//console->info("{:<30}", "left aligned");
 
-		spdlog::get("dcclite")->info("loggers can be retrieved from a global registry using the spdlog::get(logger_name)");
+		//spdlog::get("dcclite")->info("loggers can be retrieved from a global registry using the spdlog::get(logger_name)");
 
 		// Runtime log levels
-		spdlog::set_level(spdlog::level::info); // Set global log level to info
-		console->debug("This message should not be displayed!");
-		console->set_level(spdlog::level::trace); // Set specific logger's log level
-		console->debug("This message should be displayed..");
+		//spdlog::set_level(spdlog::level::info); // Set global log level to info
+		//console->debug("This message should not be displayed!");
+		combined_logger->set_level(spdlog::level::trace); // Set specific logger's log level
+		//console->debug("This message should be displayed..");
 
 		// Customize msg format for all loggers
 		spdlog::set_pattern("[%H:%M:%S %z] [%n] [%^---%L---%$] [thread %t] %v");
-		console->info("This an info message with custom format");		
+		//console->info("This an info message with custom format");		
+
+		g_spLogger = combined_logger;
 	}
 
 	extern std::shared_ptr<spdlog::logger> LogGetDefault()
 	{
-		return spdlog::get("dcclite");
+		return g_spLogger;
 	}
 }
