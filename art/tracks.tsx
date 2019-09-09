4327ab89f0c946bdd01531c1d074a3ee102a16ce
Bruno Sanches <bcsanches@gmail.com>
2019-09-09 15:12:52 -0300

    art files for future panel project

---
+++ art/tracks.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="tracks" tilewidth="16" tileheight="16" tilecount="8" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="corner_right.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="left_turnout.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="right_turnout.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="track.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="led_green.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="led_off.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="led_red.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="16" source="led_yellow.png"/>
+ </tile>
+</tileset>
---
+++ art/tracks32.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="tracks32" tilewidth="32" tileheight="32" tilecount="9" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="corner_left32.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="corner_right32.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="left_turnout32.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="right_turnout32.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="track32.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="led_green32.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="led_off32.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="led_red32.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="diagonal32.png"/>
+ </tile>
+</tileset>
