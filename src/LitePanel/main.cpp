3efe7cc72e156b18e06da77fa8d4935729b6f785
Bruno Sanches <bcsanches@gmail.com>
2019-09-14 14:18:11 -0300

    litePanel project

--- src/LitePanel/main.cpp
+++ src/LitePanel/main.cpp
@@ -1,65 +1,80 @@
 // wxWidgets "Hello World" Program
 // For compilers that support precompilation, includes "wx/wx.h".
-#define WXUSINGDLL
 #include <wx/wxprec.h>
 #ifndef WX_PRECOMP
 #include <wx/wx.h>
 #endif
-class MyApp : public wxApp
+
+class LiteApp : public wxApp
 {
-public:
-	virtual bool OnInit();
+	public:
+		virtual bool OnInit();
 };
-class MyFrame : public wxFrame
+
+class MainFrame : public wxFrame
 {
-public:
-	MyFrame();
-private:
-	void OnHello(wxCommandEvent& event);
-	void OnExit(wxCommandEvent& event);
-	void OnAbout(wxCommandEvent& event);
+	public:
+		MainFrame();
+
+	private:
+		void OnHello(wxCommandEvent& event);
+		void OnExit(wxCommandEvent& event);
+		void OnAbout(wxCommandEvent& event);
 };
+
 enum
 {
 	ID_Hello = 1
 };
-wxIMPLEMENT_APP(MyApp);
-bool MyApp::OnInit()
+
+wxIMPLEMENT_APP(LiteApp);
+bool LiteApp::OnInit()
 {
-	MyFrame* frame = new MyFrame();
+	auto *frame = new MainFrame();
 	frame->Show(true);
+
 	return true;
 }
-MyFrame::MyFrame()
-	: wxFrame(NULL, wxID_ANY, "Hello World")
+
+MainFrame::MainFrame()
+	: wxFrame(NULL, wxID_ANY, "Lite Panel")
 {
 	wxMenu* menuFile = new wxMenu;
 	menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
 		"Help string shown in status bar for this menu item");
 	menuFile->AppendSeparator();
 	menuFile->Append(wxID_EXIT);
+
 	wxMenu* menuHelp = new wxMenu;
 	menuHelp->Append(wxID_ABOUT);
+
 	wxMenuBar* menuBar = new wxMenuBar;
 	menuBar->Append(menuFile, "&File");
 	menuBar->Append(menuHelp, "&Help");
+
 	SetMenuBar(menuBar);
+
 	CreateStatusBar();
 	SetStatusText("Welcome to wxWidgets!");
-	Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
-	Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
-	Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+
+	Bind(wxEVT_MENU, &MainFrame::OnHello, this, ID_Hello);
+	Bind(wxEVT_MENU, &MainFrame::OnAbout, this, wxID_ABOUT);
+	Bind(wxEVT_MENU, &MainFrame::OnExit, this, wxID_EXIT);
 }
-void MyFrame::OnExit(wxCommandEvent& event)
+
+
+void MainFrame::OnExit(wxCommandEvent& event)
 {
 	Close(true);
 }
-void MyFrame::OnAbout(wxCommandEvent& event)
+
+void MainFrame::OnAbout(wxCommandEvent& event)
 {
 	wxMessageBox("This is a wxWidgets Hello World example",
 		"About Hello World", wxOK | wxICON_INFORMATION);
 }
-void MyFrame::OnHello(wxCommandEvent& event)
+
+void MainFrame::OnHello(wxCommandEvent& event)
 {
 	wxLogMessage("Hello world from wxWidgets!");
 }
