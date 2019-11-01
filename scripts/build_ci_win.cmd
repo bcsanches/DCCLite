ff9d99b94ffb783a1224249ee59fb6ecc63906d3
Bruno Sanches <bcsanches@gmail.com>
2019-11-01 13:03:55 -0300

    Updated build scripts for appveyor

---
+++ scripts/build_ci_win.cmd
@@ -0,0 +1,38 @@
+@echo off
+SETLOCAL
+
+set GENERATOR="Visual Studio 16 2019"
+set PLATFORM=x64
+set CONFIGURATION=Debug
+set BUILD_FOLDER=%APPVEYOR_BUILD_FOLDER%
+
+set CMAKE_BIN_x86="C:\Program Files (x86)\CMake\bin\cmake.exe"
+set CMAKE_BIN_x64="C:\Program Files\CMake\bin\cmake.exe"
+IF EXIST %CMAKE_BIN_x64% (
+	echo CMake 64-bit detected
+	set CMAKE_BIN=%CMAKE_BIN_x64%
+) ELSE (
+	IF EXIST %CMAKE_BIN_x86% (
+		echo CMake 32-bit detected
+		set CMAKE_BIN=%CMAKE_BIN_x86%
+	) ELSE (
+		echo Cannot detect either %CMAKE_BIN_x86% or
+		echo %CMAKE_BIN_x64% make sure CMake is installed
+		EXIT /B 1
+	)
+)
+echo Using CMake at %CMAKE_BIN%
+
+cd %BUILD_FOLDER%
+mkdir %BUILD_FOLDER%\build
+cd %BUILD_FOLDER%\build
+echo --- Running CMake configure ---
+%CMAKE_BIN% -G %GENERATOR% -A %PLATFORM% %BUILD_FOLDER%
+
+echo --- Building DCCLite ---
+%CMAKE_BIN% --build . --config %CONFIGURATION%
+%CMAKE_BIN% --build . --target install --config %CONFIGURATION%
+
+echo Done!
+
+ENDLOCAL
