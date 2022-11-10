d61a57ad939e55522b9e0854431a3898470ec007
bcsanches <bcsanches@gmail.com>
2022-11-10 15:40:03 -0300

    code formatting

--- src/Embedded/lib/DecodersCommon/Console.cpp
+++ src/Embedded/lib/DecodersCommon/Console.cpp
@@ -142,7 +142,7 @@ void Console::Printf(const ConsoleFlashStringHelper_t *format, ...)
     va_list args;
     va_start(args, format);        
 
-     dcclite::PrintfImpl(SerialWrapper{}, FlashStringWrapper{ format }, args);
+    dcclite::PrintfImpl(SerialWrapper{}, FlashStringWrapper{ format }, args);
 
     va_end(args);
 }
