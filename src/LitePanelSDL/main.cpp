82916efe238f6446139e6b45092e441112f90931
bcsanches <bcsanches@gmail.com>
2023-10-12 17:36:15 -0300

    LitePanel: added main window and about window

--- src/LitePanelSDL/main.cpp
+++ src/LitePanelSDL/main.cpp
@@ -24,7 +24,125 @@ constexpr auto BUILD_NUM = DCCLITE_VERSION;
 
 static bool fExitRequested = false;
 
-//static TerminalCmd g_ShutdownCmd{ "shutdown" };
+ImVec4 g_clClearColor = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+static void ImGuiDemoFunc()
+{	
+
+	// Our state
+	static bool show_demo_window = true;
+	static bool show_another_window = false;
+
+	// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+	if (show_demo_window)
+		ImGui::ShowDemoWindow(&show_demo_window);
+
+	// 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+	{
+		static float f = 0.0f;
+		static int counter = 0;
+
+		ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+		ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+		ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+		ImGui::Checkbox("Another Window", &show_another_window);
+
+		ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+		ImGui::ColorEdit3("clear color", (float *)&g_clClearColor); // Edit 3 floats representing a color
+
+		if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+			counter++;
+		ImGui::SameLine();
+		ImGui::Text("counter = %d", counter);
+
+		ImGuiIO &io = ImGui::GetIO();
+
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+		ImGui::End();
+	}
+
+	// 3. Show another simple window.
+	if (show_another_window)
+	{
+		ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+		ImGui::Text("Hello from another window!");
+		if (ImGui::Button("Close Me"))
+			show_another_window = false;
+		ImGui::End();
+	}
+}
+
+static void ShowAboutWindow(bool *p_open)
+{	
+	if (ImGui::Begin("About Lite Panel", p_open, ImGuiWindowFlags_AlwaysAutoResize))
+	{
+		ImGui::Text("Lite Panel %s", DCCLITE_VERSION);
+		ImGui::Separator();
+		ImGui::Text("By Bruno C. Sanches");
+		ImGui::Text("Licensed under the MIT License, see LICENSE for more information.");
+	}
+
+	ImGui::End();	
+}
+
+static void RenderMainWindow()
+{	
+	static bool show_about = false;
+
+	static bool use_work_area = true;
+	static ImGuiWindowFlags flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_MenuBar;
+
+	// We demonstrate using the full viewport area or the work area (without menu-bars, task-bars etc.)
+	// Based on your use case you may want one or the other.
+	const ImGuiViewport *viewport = ImGui::GetMainViewport();
+	ImGui::SetNextWindowPos(use_work_area ? viewport->WorkPos : viewport->Pos);
+	ImGui::SetNextWindowSize(use_work_area ? viewport->WorkSize : viewport->Size);
+
+	if (ImGui::Begin("Main Window", nullptr, flags))
+	{
+		if (ImGui::BeginMenuBar())
+		{
+			if (ImGui::BeginMenu("File"))
+			{
+				ImGui::MenuItem("Exit", "Ctrl+F4");
+
+				ImGui::EndMenu();
+			}
+
+			if (ImGui::BeginMenu("Edit"))
+			{				
+				ImGui::EndMenu();
+			}
+
+			if (ImGui::BeginMenu("Help"))
+			{
+				ImGui::MenuItem("About", nullptr, &show_about);
+
+				ImGui::EndMenu();
+			}
+
+			ImGui::EndMenuBar();
+		}
+
+		ImGui::Checkbox("Use work area instead of main area", &use_work_area);
+		ImGui::SameLine();
+		//HelpMarker("Main Area = entire viewport,\nWork Area = entire viewport minus sections used by the main menu bars, task bars etc.\n\nEnable the main-menu bar in Examples menu to see the difference.");
+
+		ImGui::CheckboxFlags("ImGuiWindowFlags_NoBackground", &flags, ImGuiWindowFlags_NoBackground);
+		ImGui::CheckboxFlags("ImGuiWindowFlags_NoDecoration", &flags, ImGuiWindowFlags_NoDecoration);
+		ImGui::Indent();
+		ImGui::CheckboxFlags("ImGuiWindowFlags_NoTitleBar", &flags, ImGuiWindowFlags_NoTitleBar);
+		ImGui::CheckboxFlags("ImGuiWindowFlags_NoCollapse", &flags, ImGuiWindowFlags_NoCollapse);
+		ImGui::CheckboxFlags("ImGuiWindowFlags_NoScrollbar", &flags, ImGuiWindowFlags_NoScrollbar);
+		ImGui::Unindent();		
+	}
+
+	ImGui::End();
+
+	if (show_about)
+		ShowAboutWindow(&show_about);
+}
 
 int main(int argc, char **argv)
 {				
@@ -74,6 +192,7 @@ int main(int argc, char **argv)
 		// Setup Dear ImGui context
 		IMGUI_CHECKVERSION();
 		ImGui::CreateContext();
+
 		ImGuiIO &io = ImGui::GetIO(); (void)io;
 		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
 		io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
@@ -84,12 +203,7 @@ int main(int argc, char **argv)
 
 		// Setup Platform/Renderer backends
 		ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
-		ImGui_ImplSDLRenderer3_Init(renderer);
-
-		// Our state
-		bool show_demo_window = true;
-		bool show_another_window = false;
-		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+		ImGui_ImplSDLRenderer3_Init(renderer);			
 
 		while (!fExitRequested)
 		{
@@ -122,47 +236,14 @@ int main(int argc, char **argv)
 			ImGui_ImplSDL3_NewFrame();
 			ImGui::NewFrame();
 
-			// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-			if (show_demo_window)
-				ImGui::ShowDemoWindow(&show_demo_window);
+			RenderMainWindow();
 
-			// 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
-			{
-				static float f = 0.0f;
-				static int counter = 0;
-
-				ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-				ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-				ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
-				ImGui::Checkbox("Another Window", &show_another_window);
-
-				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-				ImGui::ColorEdit3("clear color", (float *)&clear_color); // Edit 3 floats representing a color
-
-				if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-					counter++;
-				ImGui::SameLine();
-				ImGui::Text("counter = %d", counter);
-
-				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-				ImGui::End();
-			}
-
-			// 3. Show another simple window.
-			if (show_another_window)
-			{
-				ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-				ImGui::Text("Hello from another window!");
-				if (ImGui::Button("Close Me"))
-					show_another_window = false;
-				ImGui::End();
-			}
+			ImGuiDemoFunc();			
 
 			// Rendering
 			ImGui::Render();
 			
-			SDL_SetRenderDrawColor(renderer, (Uint8)(clear_color.x * 255), (Uint8)(clear_color.y * 255), (Uint8)(clear_color.z * 255), (Uint8)(clear_color.w * 255));
+			SDL_SetRenderDrawColor(renderer, (Uint8)(g_clClearColor.x * 255), (Uint8)(g_clClearColor.y * 255), (Uint8)(g_clClearColor.z * 255), (Uint8)(g_clClearColor.w * 255));
 
 			SDL_RenderClear(renderer);
 
