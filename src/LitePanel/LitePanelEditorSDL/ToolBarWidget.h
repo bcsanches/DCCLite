f0dd3849e5deba86b60d23d3bc63a6e207572ca6
bcsanches <bcsanches@gmail.com>
2023-10-21 22:24:27 -0300

    Editor widgets

---
+++ src/LitePanel/LitePanelEditorSDL/ToolBarWidget.h
@@ -0,0 +1,25 @@
+// Copyright (C) 2019 - Bruno Sanches. See the COPYRIGHT
+// file at the top-level directory of this distribution.
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+// 
+// This Source Code Form is "Incompatible With Secondary Licenses", as
+// defined by the Mozilla Public License, v. 2.0.
+
+#include "EditorWidget.h"
+
+namespace dcclite::panel_editor
+{
+	class ToolBarWidget: public EditorWidget
+	{
+		public:
+			void Display() override;
+			void Update() override;
+
+			virtual ~ToolBarWidget();
+
+		private:
+	};
+}
