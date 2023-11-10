b0c3ada0739b3648734b73b8296957569dc79650
bcsanches <bcsanches@gmail.com>
2023-11-10 15:07:23 -0300

    Drawing first grid

--- src/LitePanel/LitePanelEditorSDL/PanelEditorApp.cpp
+++ src/LitePanel/LitePanelEditorSDL/PanelEditorApp.cpp
@@ -319,20 +319,55 @@ namespace dcclite::panel_editor
 				auto panels = m_clDocument.GetPanels();
 				auto numPanels = m_clDocument.GetNumPanels();
 
+				static ImVec2 scrolling(0.0f, 0.0f);
+
 				for (int i = 0; i < numPanels; ++i)
 				{
 					if (ImGui::BeginTabItem(panels[i].GetName().c_str(), nullptr, 0))
-					{
+					{						
+						// Using InvisibleButton() as a convenience 1) it will advance the layout cursor and 2) allows us to use IsItemHovered()/IsItemActive()
 						ImVec2 canvas_p0 = ImGui::GetCursorScreenPos();      // ImDrawList API uses screen coordinates!
 						ImVec2 canvas_sz = ImGui::GetContentRegionAvail();   // Resize canvas to what's available
+						if (canvas_sz.x < 50.0f) canvas_sz.x = 50.0f;
+						if (canvas_sz.y < 50.0f) canvas_sz.y = 50.0f;
+						ImVec2 canvas_p1 = ImVec2(canvas_p0.x + canvas_sz.x, canvas_p0.y + canvas_sz.y);
 
-						ImGui::Text("Cool! work area");
+						// Draw border and background color
+						ImGuiIO &io = ImGui::GetIO();
+						ImDrawList *draw_list = ImGui::GetWindowDrawList();
+						draw_list->AddRectFilled(canvas_p0, canvas_p1, IM_COL32(50, 50, 50, 255));
+						draw_list->AddRect(canvas_p0, canvas_p1, IM_COL32(255, 255, 255, 255));
 
-						auto drawList = ImGui::GetWindowDrawList();
+						// This will catch our interactions
+						ImGui::InvisibleButton("canvas", canvas_sz, ImGuiButtonFlags_MouseButtonLeft | ImGuiButtonFlags_MouseButtonRight | ImGuiButtonFlags_MouseButtonMiddle);
+						const bool is_hovered = ImGui::IsItemHovered(); // Hovered
+						const bool is_active = ImGui::IsItemActive();   // Held
+						const ImVec2 origin(canvas_p0.x + scrolling.x, canvas_p0.y + scrolling.y); // Lock scrolled origin
+						const ImVec2 mouse_pos_in_canvas(io.MousePos.x - origin.x, io.MousePos.y - origin.y);
+						
+						// Pan (we use a zero mouse threshold when there's no context menu)
+						// You may decide to make that threshold dynamic based on whether the mouse is hovering something etc.						
+						if (is_active && ImGui::IsMouseDragging(ImGuiButtonFlags_MouseButtonRight))
+						{
+							scrolling.x += io.MouseDelta.x;
+							scrolling.y += io.MouseDelta.y;
 
-						ImU32 col_b = ImGui::GetColorU32(IM_COL32(255, 0, 255, 255));
+							if (scrolling.x < 0)
+								scrolling.x = 0;
 
-						drawList->AddLine(canvas_p0, canvas_p0 + canvas_sz, col_b);
+							if (scrolling.y < 0)
+								scrolling.y = 0;
+						}
+						
+						// Draw grid + all lines in the canvas
+						draw_list->PushClipRect(canvas_p0, canvas_p1, true);						
+
+						const float GRID_STEP = 16.0f;
+						for (float x = fmodf(scrolling.x, GRID_STEP); x < canvas_sz.x; x += GRID_STEP)
+							draw_list->AddLine(ImVec2(canvas_p0.x + x, canvas_p0.y), ImVec2(canvas_p0.x + x, canvas_p1.y), IM_COL32(200, 200, 200, 40));
+
+						for (float y = fmodf(scrolling.y, GRID_STEP); y < canvas_sz.y; y += GRID_STEP)
+							draw_list->AddLine(ImVec2(canvas_p0.x, canvas_p0.y + y), ImVec2(canvas_p1.x, canvas_p0.y + y), IM_COL32(200, 200, 200, 40));												
 
 						ImGui::EndTabItem();
 					}
