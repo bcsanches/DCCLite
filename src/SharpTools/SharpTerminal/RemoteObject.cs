4ae54f6d16986ed7c4c310f1286545a596cde7b9
bcsanches <bcsanches@gmail.com>
2024-06-02 19:06:03 -0300

    Migrated sharp terminal

--- src/SharpTools/SharpTerminal/RemoteObject.cs
+++ src/SharpTools/SharpTerminal/RemoteObject.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Json;
 using System.Linq;
-using System.Management.Instrumentation;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
